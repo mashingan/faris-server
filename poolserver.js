583d6a3dd9c127517c7f441b8117bfcb57de82b7
rdruffy <rahmat.d.ruffy@gmail.com>
2014-04-30 16:32:13 +0700

    adding several missed function etc

--- poolserver.js
+++ poolserver.js
@@ -12,60 +12,77 @@ server.on('connection', function(socket) {
   socket.on('data', function(data) {
     data = data.replace('\r\n','');
     var info = data.split(';;',2);
-    if (info[0] == 'init'){
-      var obj = JSON.parse(info[1]);
-      obj.sock = socket;
-      pool.push(obj);
-      socket.write('parsing ok\n');
-    }else if(info[0] == 'start'){
-      if(seek.length){
-        var opponent = take1from(seek);
+    switch(info[1]){
+      case 'init':
+        var obj = JSON.parse(info[1]);
+        obj.sock = socket;
+        pool.push(obj);
+        socket.write('parsing ok\n');
+        break;
+
+      case 'start':
+        var challenger = searchfrom(pool, function(x){
+          return x.sock == socket; });
+        if(seek.length > 0){
+          var opponent = take1from(seek);
+          now_playing.push({ p1: challenger, p2: opponent });
+          now_playing.push({ p1: opponent, p2: challenger });
+          opponent.sock.write('You are now playing with: '
+            +challenger.user+'.\n');
+          challenger.sock.write('You are now playing with: '
+            +opponent.user+'.\n');
+        }else{
+          socket.write('Waiting opponent\n');
+          seek.push(challenger);
+        }
+        break;
+
+      case 'play':
+        var obj = searchfrom(now_playing, function(x){
+          return x.p1.sock == socket });
+        if(obj && (obj != [])) obj.p2.sock.write(info[1]+'\n');
+        else seek.push(obj);
+        break;
+
+      case 'win':
+        console.log(searchfrom(pool, function(x){
+          return x.sock == socket; }).user+' win');
+        deletefrom(now_playing, function(x){
+          return ((x.p1.sock == socket) || (x.p2.sock == socket)); });
+        break;
+
+      case 'with':
+        var opponent;
+        for(var i in seek)
+          if(seek[i].user == info[1])
+            opponent = seek.splice(i, 1)[0];
+       
         var challenger = searchfrom(pool, function(x){
           return x.sock == socket; });
         now_playing.push({ p1: challenger, p2: opponent });
         now_playing.push({ p1: opponent, p2: challenger });
-      }else seek.push(searchfrom(pool, function(x){
-        return x.sock == socket; }));
-    }else if(info[0] == 'play'){
-      var obj = searchfrom(now_playing, function(x){
-        return x.p1.sock == socket });
-      if(obj != []) obj.p2.sock.write(info[1]+'\n');
-      else seek.push(obj);
-    }else if(info[0] == 'win'){
-      console.log(searchfrom(pool, function(x){
-        return x.sock == socket }).user+' win');
-      // bug prone, I think, let's see whether this will be stable
-      // enough
-      for(var i=0; i<now_playing.length; i++){
-        if((now_playing[i].p1.sock == socket) ||
-           (now_playing[i].p2.sock == socket))
-          now_playing.splice(i, 1);
+        break;
 
-        /* less effective version because will traverse twice
-        deletefrom(now_playing, function(x){return x.p1.sock==socket;});
-        deletefrom(now_playing, function(x){return x.p2.sock==socket;});
-        */
-      }
-    }else if(info[0] == 'with'){
-      var opponent = searchfrom(seek, function(x){
-        return x.user == info[1]; });
-      var challenger = searchfrom(pool, function(x){
-        return x.sock == socket; });
-      now_playing.push({ p1: challenger, p2: opponent });
-      now_playing.push({ p1: opponent, p2: challenger });
-    }else if(info[0] == 'quit'){
-      deletefrom(pool, function(x){ return x.sock == socket; });
-      socket.destroy();
-    }else if(info[0] == 'list'){
-      pool.forEach(function(entry){
-        if(entry.sock != socket) socket.write(entry.user+'\n');
-      });
-    }else if(info[0] == 'who'){
-      seek.forEach(function(player){
-        if(player.sock != socket) socket.write(player.user+'\n');
-      });
-    }else{
-      socket.write(data+'\n');
+      case 'quit':
+        deletefrom(pool, function(x){ return x.sock == socket; });
+        socket.destroy();
+        break;
+
+      case 'list':
+        pool.forEach(function(entry){
+          if(entry.sock != socket) socket.write(entry.user+'\n');
+        });
+        break;
+
+      case 'who':
+        seek.forEach(function(player){
+          if(player.sock != socket) socket.write(player.user+'\n');
+        });
+        break;
+
+      default:
+        socket.write(data+'\n');
+        break;
     }
   });
 
@@ -89,9 +106,7 @@ function take1from(arr){
 }
 
 function deletefrom(arr, test){
-  //using integer index version
-  //for (var i=0; i<arr.length; i++){
-  for (var i in arr){
+  for (var i=0; i<arr.length; i++){
     if(test(arr[i])) arr.splice(i, 1);
   }
 }
